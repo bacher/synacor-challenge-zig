517d0f0c476bb0218b24fb315567cd7b03adab1d
Ilia Lebedev <ilia@neon.tech>
2024-09-09 10:24:40 +0200

    add test_reader

--- src/test_reader.zig
+++ src/test_reader.zig
@@ -4,10 +4,25 @@ test "test Reader" {
     const nums = [_]u32{ 0xDEAD, 0xBEEF, 0xF000, 0xB335 };
     const nums_mem = std.mem.sliceAsBytes(&nums);
 
-    std.debug.print("{*}\n", .{&nums});
-    std.debug.print("{*}\n", .{nums_mem});
-    std.debug.print("{d}\n", .{nums.len});
-    std.debug.print("{d}\n", .{nums_mem.len});
-    std.debug.print("{}\n", .{@TypeOf(nums)});
-    std.debug.print("{}\n", .{@TypeOf(nums_mem)});
+    var stream = std.io.fixedBufferStream(nums_mem);
+    const stream_reader = stream.reader();
+
+    var buffered_reader = std.io.bufferedReader(stream_reader);
+    var numbers = std.ArrayList(u32).init(std.testing.allocator);
+    defer numbers.deinit();
+    var parsed_number_buffer: [4]u8 = undefined;
+
+    while (true) {
+        const bytes_read = try buffered_reader.read(&parsed_number_buffer);
+        if (bytes_read == 0) {
+            break;
+        }
+
+        try std.testing.expect(bytes_read == 4);
+
+        const parsed_num = std.mem.readPackedIntNative(u32, &parsed_number_buffer, 0);
+        try numbers.append(parsed_num);
+    }
+
+    std.debug.print("items = {any}\n", .{numbers.items});
 }
